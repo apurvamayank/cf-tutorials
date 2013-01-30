58aa1df2c210d2fb07cffa65df5b61db204b7519
Apurva Mayank <apurva@weboniselab.com>
2013-01-30 18:03:16 +0530

    rails-messaging-using-rabbitmq and rails-messaging-one-to-many-using-rabbitmq added

---
+++ ruby/rails-messaging-one-to-many-using-rabbitmq/app/controllers/home_controller.rb
@@ -0,0 +1,58 @@
+require 'bunny'
+require 'json'
+class HomeController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    redirect_to home_index_path(:name => params[:user][:name])
+  end
+
+  def self.amqp_url
+    services = JSON.parse(ENV['VCAP_SERVICES'], :symbolize_names => true)
+    url = services.values.map do |srvs|
+      srvs.map do |srv|
+        if srv[:label] =~ /^rabbitmq-/
+          srv[:credentials][:url]
+        else
+          []
+        end
+      end
+    end.flatten!.first
+  end
+
+
+
+  def publish
+    client = client_initialization
+    channel = client.create_channel
+    nameless_exchange = channel.fanout("test")
+    queue =   channel.queue(params[:name]).bind(nameless_exchange)
+    nameless_exchange.publish("#{params[:name]}"+"::"+"#{params[:message].to_s}")   if params[:message].present?
+    @msg = queue.pop.last
+    respond_to do |format|
+      format.js
+    end
+  end
+
+
+
+  private
+
+  def client_initialization
+    if Rails.env == "production"
+    client = Bunny.new(HomeController.amqp_url)
+    else
+      client = Bunny.new
+    end
+    client.start
+    client
+  end
+
+end
